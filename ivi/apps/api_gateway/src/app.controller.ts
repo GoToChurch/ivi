9c7e031d0236c7776bc35c5ff063fd52069de46c
Nickich <pravoslavie1488@gmail.com>
2023-05-02 15:42:11 +0300

    Docs updated

--- ivi/apps/api_gateway/src/app.controller.ts
+++ ivi/apps/api_gateway/src/app.controller.ts
@@ -6,7 +6,8 @@ import {ApiOperation, ApiQuery, ApiResponse} from "@nestjs/swagger";
 export class AppController {
   constructor(private appService: AppService) {}
 
-    @ApiOperation({summary: "Парсинг страницы всех фильмов кинопоиска и создание их сущностей в базе данных"})
+    @ApiOperation({summary: `Парсинг страницы всех фильмов кинопоиска и создание их сущностей в базе данных.
+    Существует ограничение на 200 запросов в день. Парсинг одного фильма делает 2 запроса.`})
     @ApiQuery({ name: 'from', required: false, example: 1,
       description: "С какой страницы начинать парсинг. Если не указано, то парсинг будет вестись с первой страницы"})
     @ApiQuery({ name: 'to', required: false, example: 5,
@@ -21,7 +22,8 @@ export class AppController {
         return this.appService.parseFilms(query);
     }
 
-    @ApiOperation({summary: "Парсинг страницы фильма на кинопоиске с указанным id"})
+    @ApiOperation({summary: `Парсинг страницы фильма на кинопоиске с указанным id.
+    Существует ограничение на 200 запросов в день. Парсинг одного фильма делает 2 запроса.`})
     @ApiResponse({status: 200})
     @Get('/parse/:id')
     async parseOneFilm(@Param('id') id: any) {
